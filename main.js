a88f2bb5e2b7edd685662e079abe95d4d8fd2955
Tsvetan <trendjazzer@gmail.com>
2021-03-04 17:16:11 +0100

    started creating goDown

--- main.js
+++ main.js
@@ -20,25 +20,47 @@ var mainAxis = 'row';
 var player = document.getElementsByClassName('player');
 var tail = player[0];
 var head = player[player.length - 1];
-var go = true;
+var currentAxisX = player[0].classList[1];
+var running = false;
 function newHead(axis, head) {
         if (axis == 'row') {
-                document.getElementsByClassName('row-1 col-' + (parseInt(head.classList[2].match(/\d+/)[0]) + 1))[0].classList.add('player');
+                document.getElementsByClassName(currentAxisX + ' col-' + (parseInt(head.classList[2].match(/\d+/)[0]) + 1))[0].classList.add('player');
         }
 }
 
-function move() {
+function moveX() {
         if ((parseInt(head.classList[2].match(/\d+/)[0]) + 1) == 21) {
                 alert('hai perso');
+                running = false;
                 return;
-        } else if (go) {
+        } else if (running) {
                 newHead(mainAxis, head);
                 player[0].classList.remove('player');
                 player = document.getElementsByClassName('player');
                 head = player[player.length - 1];
+        } else {
+                return;
         }
-        var timeout = setTimeout(move, 50);
+        var timeout = setTimeout(moveX, 50);
 }
 
+function goDown() {
+        var rowDown = parseInt(head.classList[1].match(/\d+/)[0]) + 1;
+        var currentCol = head.classList[2].match(/\d+/)[0];
+        head.classList.remove('player');
+        document.getElementsByClassName('row-' + rowDown + ' col-' + currentCol)[0].classList.add('player');
+}
 
-document.getElementById('help').addEventListener('click', move);
+document.getElementById('help').addEventListener('click', function() {
+        running = true;
+});
+document.getElementById('help').addEventListener('click', moveX);
+
+document.addEventListener("keydown", event => {
+        if((event.key == 's' || event.key == 'S') && running && mainAxis == 'row'){
+                running = false;
+                goDown();
+        };
+      });
+
+
